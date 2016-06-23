4567d4777043658020c904bc7943201f0be2a04b
KathyGremillet <gremillet.kathy@gmail.com>
2016-06-23 13:49:33 +0200

    Suppression du projet de base master-details qui ne sera pas utilisé

--- Urban art fair app/Urban art fair.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Urban art fair.swift
-//  Urban art fair app
-//
-//  Created by Mastere 1 IT on 22/06/2016.
-//  Copyright © 2016 Mastere 1 IT. All rights reserved.
-//
-
-import Foundation
