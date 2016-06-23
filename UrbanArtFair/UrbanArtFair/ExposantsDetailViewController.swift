cf5ad608040cfa6d305f86077f04b2b9acfef275
KathyGremillet <gremillet.kathy@gmail.com>
2016-06-23 14:16:05 +0200

    Ajout des pages details dans l'arborescence et creation des class Master et Details vides pour Planning et Exposants

---
+++ UrbanArtFair/UrbanArtFair/ExposantsDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ExposantsDetailViewController.swift
+//  UrbanArtFair
+//
+//  Created by Mastere 1 IT on 23/06/2016.
+//  Copyright © 2016 KotKot. All rights reserved.
+//
+
+import UIKit
+
+class ExposantsDetailViewController: UIViewController {
+    
+}
